9462b650edda56ce80ac9d3a1330a24610b4b187
cn <chrnicolai@gmail.com>
2013-01-24 19:29:49 +0100

    Add another missing file

---
+++ de.altimos.mdsd.majordomo/src/de/altimos/mdsd/majordomo/BoilerActor.java
@@ -0,0 +1,17 @@
+/**
+ */
+package de.altimos.mdsd.majordomo;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Boiler Actor</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see de.altimos.mdsd.majordomo.MajordomoPackage#getBoilerActor()
+ * @model
+ * @generated
+ */
+public interface BoilerActor extends HouseMountable, BooleanActor {
+} // BoilerActor
