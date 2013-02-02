13c46837c2057ea60b28ae52f0ebc88e8987dddf
Jan Graichen <jg@altimos.de>
2013-02-02 20:20:49 +0100

    Set temperatur sensor assemply default value.

--- de.altimos.mdsd.majordomo.codegen/src/de/altimos/mdsd/majordomo/simulator/assemblies/MTemperatureSensorAssembly.java
+++ de.altimos.mdsd.majordomo.codegen/src/de/altimos/mdsd/majordomo/simulator/assemblies/MTemperatureSensorAssembly.java
@@ -8,6 +8,7 @@ public class MTemperatureSensorAssembly extends MFloatSensorAssembly {
 		getSpinnerModel().setMinimum(-20.0);
 		getSpinnerModel().setMaximum(80.0);
 		getSpinnerModel().setStepSize(1.0);
+		getSpinnerModel().setValue(18.0);
 	}
 
 }
