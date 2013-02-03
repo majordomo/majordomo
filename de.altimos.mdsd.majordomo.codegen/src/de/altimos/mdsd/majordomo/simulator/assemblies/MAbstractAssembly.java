ca99f0e075b162e1af20e06413178c215e4affb5
Jan Graichen <jg@altimos.de>
2013-02-03 15:30:11 +0100

    Add icons to simulator.

--- de.altimos.mdsd.majordomo.codegen/src/de/altimos/mdsd/majordomo/simulator/assemblies/MAbstractAssembly.java
+++ de.altimos.mdsd.majordomo.codegen/src/de/altimos/mdsd/majordomo/simulator/assemblies/MAbstractAssembly.java
@@ -1,9 +1,13 @@
 package de.altimos.mdsd.majordomo.simulator.assemblies;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.net.URL;
 
-import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
@@ -13,6 +17,7 @@ public abstract class MAbstractAssembly implements MAssembly {
 	private String name;
 	private JPanel ui;
 	private TitledBorder border;
+	private URL icon;
 
 	public MAbstractAssembly(String name) {
 		this.name = name;
@@ -22,12 +27,18 @@ public abstract class MAbstractAssembly implements MAssembly {
 		return name;
 	}
 	
+	public void setIcon(URL icon) {
+		this.icon = icon;
+	}
+	
 	public JPanel getUI() {
 		if(ui == null) {
 			ui = new JPanel();
 			ui.setLayout(new GridLayout(1, 1));
 			
-			border = BorderFactory.createTitledBorder(getName());
+			if(icon == null) icon = ClassLoader.getSystemResource("icons/Action.gif");
+			border = new IcononicTitledBorder(getName(), 
+					new ImageIcon(icon).getImage());
 			ui.setBorder(border);
 			
 			ui.add(getAssemblyComponent());
@@ -46,4 +57,22 @@ public abstract class MAbstractAssembly implements MAssembly {
 	
 	abstract protected JComponent getAssemblyComponent();
 
+	static public class IcononicTitledBorder extends TitledBorder {
+
+		private static final long serialVersionUID = 798207293937530606L;
+		private Image img;
+
+		public IcononicTitledBorder(String title, Image img) {
+			super("    " + title);
+			this.img = img;
+		}
+		
+		@Override
+		public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) 
+		{
+			super.paintBorder(c, g, x, y, width, height);
+			
+			g.drawImage(img, 7, -1, img.getWidth(null), img.getHeight(null), null);
+		}
+	}
 }
